aff1453053eb961ec7f2679b8d8972c9cdaf7b3c
ANDRÉ RODRIGUES SANTOS <69979784+andredesenvolvedorbnu@users.noreply.github.com>
2026-09-15 11:21:25 -0300

    feat: add shared publication detail route

---
+++ src/app/(app)/publicacao/[id]/page.tsx
@@ -0,0 +1,8 @@
+import { PostDetailClient } from '@/components/post-detail-client';
+
+type PageProps={params:Promise<{id:string}>};
+
+export default async function Page({params}:PageProps){
+  const {id}=await params;
+  return <PostDetailClient postId={id}/>;
+}
