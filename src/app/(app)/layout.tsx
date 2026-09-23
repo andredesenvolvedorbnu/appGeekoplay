9ec7def0aef3d098e05ef1f8085434ff202f5d05
ANDRÉ RODRIGUES SANTOS <69979784+andredesenvolvedorbnu@users.noreply.github.com>
2026-09-23 11:05:28 -0300

    feat: surface status benefit notices

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,13 @@
 import { AppShell } from '@/components/app-shell';
 import { LevelUpWatcher } from '@/components/level-up-watcher';
 import { ThemeToggle } from '@/components/theme-toggle';
+import { StatusBenefitNotice } from '@/components/status-benefit-notice';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return <AppShell>
     <ThemeToggle/>
     <LevelUpWatcher/>
+    <StatusBenefitNotice/>
     {children}
   </AppShell>;
 }
